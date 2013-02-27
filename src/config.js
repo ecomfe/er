b21a8fa81cb7e936315dc03038823af1e354214d
otakustay <otakustay@live.com>
2013-02-27 13:21:13 +0800

    Removed unused configuration items

--- src/config.js
+++ src/config.js
@@ -8,11 +8,9 @@
 define(
     'config',
     {
-        iframeId: 'er-locator-iframe',
         mainElement: 'main',
         indexURL: '/',
         noAuthorityLocation: '/401',
-        notFoundLocation: '/404',
-        defaultModule: 'base'
+        notFoundLocation: '/404'
     }
 );
