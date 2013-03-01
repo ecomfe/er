b2ec8758938b1be1c621d202f1d456f8f611f33b
otakustay <otakustay@live.com>
2013-03-01 14:08:28 +0800

    Added main entry module with a convenient start() method to start controller, router and locator object

---
+++ src/main.js
@@ -0,0 +1,14 @@
+define(
+    'main',
+    function(require) {
+        return {
+            version: '${version}',
+
+            start: function() {
+                require('./controller').start();
+                require('./router').start();
+                require('./locator').start();
+            }
+        };
+    }
+);
