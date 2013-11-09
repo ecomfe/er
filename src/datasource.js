0d2a1ecb77954af52df03e1bcf3543a3aa5a6c9b
otakustay <otakustay@gmail.com>
2013-11-09 23:30:23 +0800

    Added more utility methods to datasource
    Fixed #68

--- src/datasource.js
+++ src/datasource.js
@@ -62,6 +62,56 @@ define(
             };
         };
 
+        /**
+         * 当属性为`null`或`undefined`时，使用默认值代替
+         *
+         * @param {Mixed} defaultValue 用于代替的默认值
+         * @param {string} [name] 判断的属性名，默认与当前获取的属性名相同
+         * @return {function} 数据获取函数
+         */
+        datasource.defaultValue = function (defaultValue, name) {
+            return function (model, options) {
+                if (!options.name && !name) {
+                    throw new Error('No property name specified '
+                        + 'to determine whether value exists in this model');
+                }
+
+                var propertyName = name || options.name;
+                return model.hasValue(propertyName)
+                    ? model.get(propertyName)
+                    : defaultValue;
+            };
+        };
+
+        /**
+         * 转换属性类型
+         *
+         * @param {string} type 转换的目标类型，支持`number`、`string`或`boolean`
+         * @param {string} [name] 指定属性名，默认与当前获取的属性名相同
+         * @return {function} 数据获取函数
+         */
+        datasource.convertTo = function (type, name) {
+            return function (mode, options) {
+                if (!options.name && !name) {
+                    throw new Error('No property name specified to convert');
+                }
+
+                var property = name || options.name;
+                var value = model.get(property);
+
+                switch (type) {
+                    case 'number':
+                        return parseInt(value, 10);
+                    case 'string':
+                        return value + '';
+                    case 'boolean':
+                        return !!value;
+                    default:
+                        return value;
+                }
+            };
+        };
+
         return datasource;
     }
 );
