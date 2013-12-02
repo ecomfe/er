4df3e4df82edcabb88192466d2b206367f41c25b
otakustay <otakustay@gmail.com>
2013-12-02 18:36:32 +0800

    添加etpl依赖
    Fixed #65

--- src/tpl.js
+++ src/tpl.js
@@ -1,25 +1,12 @@
 /**
  * ER (Enterprise RIA)
  * Copyright 2013 Baidu Inc. All rights reserved.
- * 
+ *
  * @file 加载template的AMD插件
  * @author otakustay
  */
 define(
     function (require) {
-        var plugin = {
-            load: function (resourceId, parentRequire, load) {
-                function addTemplate(text) {
-                    var template = require('./template');
-                    template.parse(text);
-                    load(text);
-                }
-
-                var url = parentRequire.toUrl(resourceId);
-                require('./ajax').get(url, null, true).then(addTemplate);
-            }
-        };
-
-        return plugin;
+        return require('etpl/tpl');
     }
 );
