cfe4a946656677314df5993a7dd5f9a1c7eaf0e3
otakustay <otakustay@gmail.com>
2014-02-11 12:25:01 +0800

    移除无用代码

--- src/Action.js
+++ src/Action.js
@@ -91,12 +91,6 @@ define(
 
             this.model = this.createModel(args);
 
-            // TODO: 确认影响后删除这一块，由于允许任何对象当数据模型，不能依赖`fill`
-            var Model = require('./Model');
-            if (this.model instanceof Model) {
-                this.model.fill(args);
-            }
-
             if (this.model && typeof this.model.load === 'function') {
                 var loadingModel = this.model.load();
                 return loadingModel.then(
