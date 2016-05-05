9d7ae59869538b3fb0d34a248b390fce819ab30d
baidu <baidu@dengxiaohongdeMacBook-Pro.local>
2016-05-05 21:31:34 +0800

    预防Model和View中原型上有引用类型因共享修改导致的问题

--- src/View.js
+++ src/View.js
@@ -9,6 +9,11 @@
 define(
     function (require) {
         var util = require('./util');
+        var u = require('underscore');
+
+        function create(oringin) {
+            return Object.create ? Object.create(oringin) : u.clone(oringin);
+        }
 
         /**
          * @class View
@@ -26,6 +31,17 @@ define(
         var exports = {};
 
         exports.constructor = function () {
+
+            // 如果prototype上的属性是引用类型，则复制一份，
+            // 防止因共享修改导致的问题
+            if (!this.hasOwnProperty('uiProperties') && this.uiProperties) {
+                this.uiProperties = create(this.uiProperties);
+            }
+
+            if (!this.hasOwnProperty('uiEvents') && this.uiEvents) {
+                this.uiEvents = create(this.uiEvents);
+            }
+
             this.initialize();
         };
 
